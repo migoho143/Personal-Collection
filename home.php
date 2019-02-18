5c2f03f22c3e534bf3e5218f97ef5969afb1e915
migoho143 <bacledevon143@gmail.com>
2019-02-18 11:15:44 -0800

    first commit

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Form in Design </title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" href="bootstrap-4.0.0-beta.3-dist/css/bootstrap.min.css">
+		
+	</head>
+<body>
+<div class="header1">
+ <a style="position:absolute;bottom:40%;right:45%;" href="login.php" class="button1">Get Started</a>
+</div>
+ 
+
+
+
+
+</body>
+</html>
